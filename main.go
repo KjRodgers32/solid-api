ea848a9bc3773780258cc617dc8ebd4a492af93e
Kevin <kjrodgers26@gmail.com>
2025-04-29 00:47:53 -0400

    fleshed out get and post request for users data

--- main.go
+++ main.go
@@ -1,16 +1,51 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/jackc/pgx/v5"
+
 	"github.com/lpernett/godotenv"
+
+	"github.com/KjRodgers32/solid-api/db"
 )
 
 type API struct {
 	addr string
+}
+
+type DBConfig struct {
+	Name             string
+	User             string
+	Pass             string
+	Port             string
+	Host             string
+	ConnectionString string
+}
+
+type UserJSON struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+type UsersData struct {
+	Status string    `json:"status"`
+	Data   []db.User `json:"data"`
+}
+
+func loadDBConfig() DBConfig {
+	return DBConfig{
+		Name: os.Getenv("DB"),
+		User: os.Getenv("DB_USER"),
+		Pass: os.Getenv("DB_PASS"),
+		Port: os.Getenv("DB_PORT"),
+		Host: os.Getenv("DB_HOST"),
+	}
 }
 
 func loggingMiddleware(f http.HandlerFunc) http.HandlerFunc {
@@ -19,10 +54,25 @@ func loggingMiddleware(f http.HandlerFunc) http.HandlerFunc {
 		f(w, r)
 	}
 }
+
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("error loading .env file: %s", err)
 	}
+
+	dbConfig := loadDBConfig()
+	connectionString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", dbConfig.User, dbConfig.Pass, dbConfig.Host, dbConfig.Port, dbConfig.Name)
+	ctx := context.Background()
+
+	conn, err := pgx.Connect(ctx, connectionString)
+	if err != nil {
+		log.Fatal("error connecting to database:", err)
+	}
+
+	defer conn.Close(ctx)
+
+	queries := db.New(conn)
+
 	port := os.Getenv("SERVER_PORT")
 
 	api := &API{
@@ -37,9 +87,43 @@ func main() {
 	mux.HandleFunc("/api/v1/users", loggingMiddleware(func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
-			fmt.Fprint(w, "returning all users in db")
+			users, err := queries.ListUsers(ctx)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+
+			responseData := UsersData{
+				Status: "OK",
+				Data:   users,
+			}
+
+			if err = json.NewEncoder(w).Encode(responseData); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
 		case http.MethodPost:
-			http.Error(w, "adding users not implemented yet", http.StatusNotImplemented)
+			var user db.User
+			if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+			}
+			addedUser, err := queries.CreateUser(ctx, user.Name)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+
+			responseData := UserJSON{
+				ID:   addedUser.ID,
+				Name: addedUser.Name,
+			}
+
+			if err = json.NewEncoder(w).Encode(responseData); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
 		case http.MethodPut, http.MethodPatch:
 			http.Error(w, "updating users not implemented yet", http.StatusNotImplemented)
 		case http.MethodDelete:
